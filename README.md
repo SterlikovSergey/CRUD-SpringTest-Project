57a2e0a39c04afdf1ca43e82fecd591b78fcee34
St.Sergey.Minsky <Sterlikovsergey@icloud.com>
2024-09-11 12:38:51 +0300

    add project initialization from main branch

---
+++ README.md
@@ -0,0 +1,12 @@
+# CRUD-SpringTest-Project 
+
+## Complete the task:
+
+Создать CRUD Spring Boot приложение и покрыть его тестами. 
+### Использовать в том числе 
+1. @ParameterizedTest
+2. MockMvc
+3. Тесты на мапперы
+4. Посмотреть на assertThat
+5. test coverage
+6. @Captor
---
+++ src/main/java/ru/clevertec/crudspringtestproject/CrudSpringTestProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.crudspringtestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSpringTestProjectApplication.class, args);
+    }
+
+}
